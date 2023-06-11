6f3cd4fe6fa8f7e09e9488673550da9e712ea5fc
Serhii <sserhiivalko@gmail.com>
2023-06-11 19:41:59 +0300

    c

--- index.js
+++ index.js
@@ -1,126 +1,58 @@
 const express = require('express');
-const fs = require('fs');
+const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Middleware для обработки тела запроса в формате JSON
-app.use(express.json());
+app.use(bodyParser.json());
+
+let users = [];
+
+// Получение списка пользователей
 app.get('/users', (req, res) => {
-    fs.readFile('users.json', 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-            res.status(500).send('Ошибка сервера', err);
-        } else {
-            const users = JSON.parse(data);
-            res.json(users);
-        }
-    });
+    res.json(users);
 });
 
-// GET запрос для получения информации о конкретном пользователе
+// Получение информации о конкретном пользователе
 app.get('/users/:id', (req, res) => {
     const userId = req.params.id;
-
-    fs.readFile('users.json', 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-            res.status(500).send('Ошибка сервера');
-        } else {
-            const users = JSON.parse(data);
-            const user = users.find((u) => u.id === userId);
-
-            if (user) {
-                res.json(user);
-            } else {
-                res.status(404).send('Пользователь не найден');
-            }
-        }
-    });
+    const user = users.find((u) => u.id === userId);
+    if (user) {
+        res.json(user);
+    } else {
+        res.status(404).send('Пользователь не найден');
+    }
 });
 
-// POST запрос для создания нового пользователя
+// Создание нового пользователя
 app.post('/users', (req, res) => {
     const newUser = req.body;
-
-    fs.readFile('users.json', 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-            res.status(500).send('Ошибка сервера');
-        } else {
-            const users = JSON.parse(data);
-            users.push(newUser);
-
-            fs.writeFile('users.json', JSON.stringify(users), (err) => {
-                if (err) {
-                    console.error(err);
-                    res.status(500).send('Ошибка сервера');
-                } else {
-                    res.send('Пользователь успешно создан');
-                }
-            });
-        }
-    });
+    users.push(newUser);
+    res.send('Пользователь успешно создан');
 });
 
-// PUT запрос для обновления информации о пользователе
+// Обновление информации о пользователе
 app.put('/users/:id', (req, res) => {
     const userId = req.params.id;
     const updatedUser = req.body;
-
-    fs.readFile('users.json', 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-            res.status(500).send('Ошибка сервера');
-        } else {
-            let users = JSON.parse(data);
-            const userIndex = users.findIndex((u) => u.id === userId);
-
-            if (userIndex !== -1) {
-                users[userIndex] = { ...users[userIndex], ...updatedUser };
-
-                fs.writeFile('users.json', JSON.stringify(users), (err) => {
-                    if (err) {
-                        console.error(err);
-                        res.status(500).send('Ошибка сервера');
-                    } else {
-                        res.send('Информация о пользователе обновлена');
-                    }
-                });
-            } else {
-                res.status(404).send('Пользователь не найден');
-            }
-        }
-    });
+    const userIndex = users.findIndex((u) => u.id === userId);
+    if (userIndex !== -1) {
+        users[userIndex] = { ...users[userIndex], ...updatedUser };
+        res.send('Информация о пользователе обновлена');
+    } else {
+        res.status(404).send('Пользователь не найден');
+    }
 });
 
-// DELETE запрос для удаления пользователя
+// Удаление пользователя
 app.delete('/users/:id', (req, res) => {
     const userId = req.params.id;
-
-    fs.readFile('users.json', 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-            res.status(500).send('Ошибка сервера');
-        } else {
-            let users = JSON.parse(data);
-            const userIndex = users.findIndex((u) => u.id === userId);
-
-            if (userIndex !== -1) {
-                users.splice(userIndex, 1);
-
-                fs.writeFile('users.json', JSON.stringify(users), (err) => {
-                    if (err) {
-                        console.error(err);
-                        res.status(500).send('Ошибка сервера');
-                    } else {
-                        res.send('Пользователь удален');
-                    }
-                });
-            } else {
-                res.status(404).send('Пользователь не найден');
-            }
-        }
-    });
+    const userIndex = users.findIndex((u) => u.id === userId);
+    if (userIndex !== -1) {
+        users.splice(userIndex, 1);
+        res.send('Пользователь удален');
+    } else {
+        res.status(404).send('Пользователь не найден');
+    }
 });
 
 // Запуск сервера
